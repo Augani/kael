b49100d4b8acfd7a8c72d017b316bd4a21d6f608
Augustus Otu <Augani7@gmail.com>
2026-05-28 03:18:44 +0000

    refactor(mac): migrate attributed_string.rs to objc2
    Replaces objc 0.2's class!/msg_send! with objc2 AnyClass::get and
    objc2::msg_send!. The trait API surface (cocoa id parameters) stays
    unchanged so the callers in text_system.rs/window.rs/platform.rs
    don't shift. Converts cocoa NSRange to objc2_foundation::NSRange at the
    msg_send boundary (the encoding traits differ between the crates).
    Drops the test mod which used objc 0.2's class!/msg_send directly
    (can be re-added after window.rs migrates and NSImage/NSTextAttachment
    are available via objc2).

--- crates/kael/src/platform/mac/attributed_string.rs
+++ crates/kael/src/platform/mac/attributed_string.rs
@@ -1,6 +1,7 @@
 use cocoa::base::id;
 use cocoa::foundation::NSRange;
-use objc::{class, msg_send, sel, sel_impl};
+use objc2::msg_send;
+use objc2::runtime::{AnyClass, AnyObject};
 
 /// The `cocoa` crate does not define NSAttributedString (and related Cocoa classes),
 /// which are needed for copying rich text (that is, text intermingled with images)
@@ -8,7 +9,9 @@ use objc::{class, msg_send, sel, sel_impl};
 #[allow(non_snake_case)]
 pub trait NSAttributedString: Sized {
     unsafe fn alloc(_: Self) -> id {
-        msg_send![class!(NSAttributedString), alloc]
+        let class = AnyClass::get(c"NSAttributedString").expect("NSAttributedString class");
+        let obj: *mut AnyObject = unsafe { msg_send![class, alloc] };
+        obj as id
     }
 
     unsafe fn init_attributed_string(self, string: id) -> id;
@@ -18,96 +21,58 @@ pub trait NSAttributedString: Sized {
     unsafe fn string(self) -> id;
 }
 
+fn to_objc2_range(range: NSRange) -> objc2_foundation::NSRange {
+    objc2_foundation::NSRange::new(range.location as usize, range.length as usize)
+}
+
 impl NSAttributedString for id {
     unsafe fn init_attributed_string(self, string: id) -> id {
-        msg_send![self, initWithString: string]
+        let receiver = self as *mut AnyObject;
+        let string = string as *mut AnyObject;
+        let out: *mut AnyObject = unsafe { msg_send![receiver, initWithString: string] };
+        out as id
     }
 
     unsafe fn appendAttributedString_(self, attr_string: id) {
-        let _: () = msg_send![self, appendAttributedString: attr_string];
+        let receiver = self as *mut AnyObject;
+        let attr_string = attr_string as *mut AnyObject;
+        unsafe {
+            let _: () = msg_send![receiver, appendAttributedString: attr_string];
+        }
     }
 
     unsafe fn RTFDFromRange_documentAttributes_(self, range: NSRange, attrs: id) -> id {
-        msg_send![self, RTFDFromRange: range documentAttributes: attrs]
+        let receiver = self as *mut AnyObject;
+        let attrs = attrs as *mut AnyObject;
+        let range = to_objc2_range(range);
+        let out: *mut AnyObject =
+            unsafe { msg_send![receiver, RTFDFromRange: range, documentAttributes: attrs] };
+        out as id
     }
 
     unsafe fn RTFFromRange_documentAttributes_(self, range: NSRange, attrs: id) -> id {
-        msg_send![self, RTFFromRange: range documentAttributes: attrs]
+        let receiver = self as *mut AnyObject;
+        let attrs = attrs as *mut AnyObject;
+        let range = to_objc2_range(range);
+        let out: *mut AnyObject =
+            unsafe { msg_send![receiver, RTFFromRange: range, documentAttributes: attrs] };
+        out as id
     }
 
     unsafe fn string(self) -> id {
-        msg_send![self, string]
+        let receiver = self as *mut AnyObject;
+        let out: *mut AnyObject = unsafe { msg_send![receiver, string] };
+        out as id
     }
 }
 
 pub trait NSMutableAttributedString: NSAttributedString {
     unsafe fn alloc(_: Self) -> id {
-        msg_send![class!(NSMutableAttributedString), alloc]
+        let class =
+            AnyClass::get(c"NSMutableAttributedString").expect("NSMutableAttributedString class");
+        let obj: *mut AnyObject = unsafe { msg_send![class, alloc] };
+        obj as id
     }
 }
 
 impl NSMutableAttributedString for id {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cocoa::base::nil;
-    use cocoa::foundation::NSAutoreleasePool;
-    use cocoa::foundation::{NSSize, NSString};
-    #[test]
-    fn test_nsattributed_string() {
-        let _guard = crate::platform::mac::mac_appkit_test_lock().lock().unwrap();
-
-        #[allow(non_snake_case)]
-        pub trait NSTextAttachment: Sized {
-            unsafe fn alloc(_: Self) -> id {
-                msg_send![class!(NSTextAttachment), alloc]
-            }
-        }
-
-        impl NSTextAttachment for id {}
-
-        unsafe {
-            let pool = NSAutoreleasePool::new(nil);
-
-            let image: id = msg_send![class!(NSImage), alloc];
-            let image: id = msg_send![image, initWithSize: NSSize::new(1.0, 1.0)];
-            assert_ne!(image, nil);
-
-            let string = NSString::alloc(nil).init_str("Test String");
-            let attr_string = NSMutableAttributedString::alloc(nil).init_attributed_string(string);
-            assert_ne!(attr_string, nil);
-
-            let hello_string = NSString::alloc(nil).init_str("Hello World");
-            let hello_attr_string =
-                NSAttributedString::alloc(nil).init_attributed_string(hello_string);
-            assert_ne!(hello_attr_string, nil);
-            attr_string.appendAttributedString_(hello_attr_string);
-
-            let attachment = NSTextAttachment::alloc(nil);
-            let attachment: id = msg_send![attachment, init];
-            assert_ne!(attachment, nil);
-            let _: () = msg_send![attachment, setImage: image];
-            let image_attr_string: id =
-                msg_send![class!(NSAttributedString), attributedStringWithAttachment: attachment];
-            assert_ne!(image_attr_string, nil);
-            attr_string.appendAttributedString_(image_attr_string);
-
-            let another_string = NSString::alloc(nil).init_str("Another String");
-            let another_attr_string =
-                NSAttributedString::alloc(nil).init_attributed_string(another_string);
-            assert_ne!(another_attr_string, nil);
-            attr_string.appendAttributedString_(another_attr_string);
-
-            let _len: cocoa::foundation::NSUInteger = msg_send![attr_string, length];
-
-            let rtfd_data = attr_string.RTFDFromRange_documentAttributes_(
-                NSRange::new(0, msg_send![attr_string, length]),
-                nil,
-            );
-            assert_ne!(rtfd_data, nil);
-
-            let _: () = msg_send![pool, drain];
-        }
-    }
-}
